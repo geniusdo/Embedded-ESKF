270a6e45bb12cde765bd609786729e3f1f2c3244
geniusdo <2403644992@qq.com>
2024-12-23 04:09:03 +0800

    update demo

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <unistd.h>
 #include "config.h"
 #include "ConsistentOrientationFilter.hpp"
+
+using namespace EmbeddedMath;
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +21,17 @@ int main(int argc, char *argv[])
 
     std::getline(file, line);
 
+    // ESKF initalization
+    const double gyro_noise = 1e-6;
+    const double gyro_random_walk = 1e-6;
+    const double gravity_noise = 1e-3;
+    auto consistent_filter = Filter::ConsistentOrientation::ConsistentOrientationFilter();
+    consistent_filter.setImuParam(Vector3d(gyro_noise, gyro_noise, gyro_noise), 400);
+    consistent_filter.setMeasurementParam(Vector3d(gravity_noise, gravity_noise, gravity_noise));
+    consistent_filter.setInitialState(Quaterniond::Identity());
+
+    int cnt = 0;
+
     while (std::getline(file, line))
     {
         std::istringstream ss(line);
@@ -27,15 +41,23 @@ int main(int argc, char *argv[])
 
         if (ss >> timestamp)
         {
-            char comma; 
+            char comma;
             ss >> comma >> acce_x >> comma >> acce_y >> comma >> acce_z >> comma >> gyro_x >> comma >> gyro_y >> comma >> gyro_z;
 
-            std::cout << "Timestamp: " << timestamp << " acce_x: " << acce_x << " acce_y: " << acce_y << " acce_z: " << acce_z << " gyro_x: " << gyro_x << " gyro_y: " << gyro_y << " gyro_z: " << gyro_z << std::endl;
+            Vector4d input = Vector4d(0.0025, gyro_x, gyro_y, gyro_z);
+
+            auto consistent_prediction = consistent_filter.predict(input);
+
+            Vector3d acce = Vector3d(acce_x, acce_y, acce_z);
+            consistent_filter.update(acce.normalized());
+            std::cout << "Quaternion " << consistent_prediction.w() << " " << consistent_prediction.x() << " " << consistent_prediction.y() << " " << consistent_prediction.z() << std::endl;
         }
         else
         {
             std::cerr << "Error parsing line: " << line << std::endl;
         }
+        // usleep(1000);
+        cnt++;
     }
 
     file.close();
